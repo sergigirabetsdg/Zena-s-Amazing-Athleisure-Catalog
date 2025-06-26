e2a6180d68fdab6d9f37bd672e80550cce230c03
sergigirabetsdg <sergi.girabet@sdggroup.com>
2025-06-26 13:12:47 +0200

    Update streamlit_app.py
    changed some lines for a better visualitzation and performance of the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,40 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
 import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
 
 session = get_active_session()
 
-# get a list of colors for a drop list selection
-table_colors = session.sql("select color_or_style from catalog_for_website")
+# Get a list of colors for a drop list selection
+table_colors = session.sql("SELECT color_or_style FROM catalog_for_website")
 pd_colors = table_colors.to_pandas()
 
-# Oyt the list of colors into a drop list selector 
+# Put the list of colors into a drop list selector
 option = st.selectbox('Pick a sweatsuit color or style:', pd_colors)
 
-# We'll build the image caption now, since we can
+# Build the image caption
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
-# use the color selected to go back and get all the info from the database
-table_prod_data = session.sql("select file_name, price, size_list, upsell_product_desc, file_url from catalog_for_website where color_or_style = '" + option + "';")
-pd_prod_data = table_prod_data.to_pandas() 
+# Use the color selected to query product details safely
+query = """
+    SELECT file_name, price, size_list, upsell_product_desc, file_url
+    FROM catalog_for_website
+    WHERE color_or_style = ?
+"""
+table_prod_data = session.sql(query, params=[option])
+pd_prod_data = table_prod_data.to_pandas()
 
-# assign each column of the row returned to its own variable 
-price = '$' + str(pd_prod_data['PRICE'].iloc[0])+'0'
+# Extract data fields from query result
+price = '${:.2f}'.format(pd_prod_data['PRICE'].iloc[0])
 file_name = pd_prod_data['FILE_NAME'].iloc[0]
 size_list = pd_prod_data['SIZE_LIST'].iloc[0]
 upsell = pd_prod_data['UPSELL_PRODUCT_DESC'].iloc[0]
 url = pd_prod_data['FILE_URL'].iloc[0]
 
-# display the info on the page
+# Display product info
 st.image(image=url, width=400, caption=product_caption)
-st.markdown('**Price:** '+ price)
+st.markdown('**Price:** ' + price)
 st.markdown('**Sizes Available:** ' + size_list)
 st.markdown('**Also Consider:** ' + upsell)
-
-
-st.write(url)
+st.markdown(f"[Download Image]({url})")
